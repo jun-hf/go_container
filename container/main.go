73bdbbfab652dc7ea205db6c0b0b5f80f9712886
jun-hf <86782267+jun-hf@users.noreply.github.com>
2024-03-16 15:13:50 +0000

    add mount file

--- container/main.go
+++ container/main.go
@@ -2,32 +2,55 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
 	"os/exec"
 	"syscall"
+	"log"
 )
 
 func main() {
-	switch command := os.Args[1]; command {
+	switch os.Args[1]{
 	case "run":
 		run()
+	case "child":
+		child()
 	default:
-		fmt.Println("Command not found")
+		fmt.Println("COMMAND not found")
 	}
 }
 
 func run() {
-	fmt.Println("Running")
-	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
 	cmd.Stderr = os.Stderr
-	cmd.Stdout = os.Stdout
 	cmd.Stdin = os.Stdin
-	cmd.SysProcAttr = &syscall.SysProcAttr {
+	cmd.Stdout = os.Stdout
+
+	cmd.SysProcAttr = &syscall.SysProcAttr{
 		Cloneflags: syscall.CLONE_NEWUTS,
 	}
+
+	logerror(cmd.Run())
+}
+
+func child() {
+	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+	cmd.Stderr = os.Stderr
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	syscall.Sethostname([]byte("container"))
+	syscall.Chroot("/home/ubuntu")
+	os.Chdir("/home/ubuntu")
+	syscall.Mount("proc", "proc", "proc", 0, "")
+	syscall.Mount("thing", "mytemp", "tmpfs", 0, "")
 	err := cmd.Run()
 	if err != nil {
 		log.Fatalf("cmd.Run() failed with %s\n", err)
 	}
 }
+
+func logerror(e error) {
+	if e != nil {
+		log.Fatalf("Failed with %s\n", e)
+	}
+}
+
